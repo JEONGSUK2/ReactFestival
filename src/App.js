48c7fb7efc559f4cd03f5f54c7c5db66f3170ead
이정석 <dlwjdtjr3088@naver.com>
2023-09-05 15:44:22 +0900

    20230925 필터,map,스타일 컴포넌트, 폰트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import GlobalStyle from "./component/GlobalStyle";
+import Main from "./pages/Main";
+import Detail from "./pages/Detail";
+import Aside from "./component/Aside";
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
+// import Example from "./pages/Example";
+// import Example2 from "./pages/Example2";
+// import Example3 from "./pages/Example3";
+
 
 function App() {
+  const light = {
+    colors: {
+      Primary : "orange",
+      Secondary : "orangered"
+    }
+  }
+  const dark = {
+    colors : {
+      Primary : "#272929",
+      Secondary : "#e9e9e9"
+    }
+  }
+
+  const [themeConfig, setThemeConfig] = useState("light")
+  const DarkMode = themeConfig === 'light' ? light : dark;
+  const ThemeSelect = ()=>{
+    setThemeConfig(themeConfig === 'light' ? 'dark' : 'light')
+  }
+// 어떠한 기능을 만들었다면 보내기 위해 routing을 해줘야 한다 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ThemeProvider theme={DarkMode} >
+    <GlobalStyle />
+    <Aside ThemeSelect={ThemeSelect} themeConfig={themeConfig} />
+    <ul>
+      {/* <li><NavLink to="/">홈</NavLink></li>
+      <li><NavLink to="/detail">디테일</NavLink></li> */}
+    </ul>
+      <Routes>
+        {/* <Route path="/" element={<Main/>}></Route> */}
+        {/* /의 의미는 화면에 접속할때 먼저 보여줄 페이지 */}
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/detail" element={<Detail/>}></Route>
+      </Routes>
+      </ThemeProvider>
+    </>
   );
 }
-
 export default App;
---
+++ src/pages/Example3.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import Example3_ from './../component/Example3'
+import { styled } from 'styled-components'
+
+const Content = styled.div`
+  display: flex;
+  width: 500px;
+  justify-content: space-between;
+
+`
+const ContentItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width:500px;
+`
+
+const Count = styled.div`
+  display: flex;
+  width: 500px;
+  justify-content: space-between;
+  
+`
+
+
+function Example3() {
+    let [ data, setData] = useState(Example3_)
+    let [animal, setAnimal] = useState("전체")
+
+    const dataFilter = data.filter(e=>{
+      if(animal === "전체"){
+        return e.animal
+      }else{
+        return e.animal === animal
+      }
+    })
+  
+const animaltype = [...new Set(data.map(e=>e.animal))]
+   console.log(animaltype)
+  return (
+   <Content>
+    <ul>
+      <li onClick={()=>{setAnimal("전체")}}>전체</li>
+      {
+        animaltype.map((e,i)=>{
+          return(
+            <ContentItem>
+            <li key={i} onClick={()=>setAnimal(e)}>{e}</li>
+            </ContentItem>
+          )
+        })
+      }
+    </ul>
+    <Count>
+      {
+        dataFilter.map((el,i)=>{
+          return(
+            <>
+            <p key={i}>{el. height}</p>
+            <p>{el.gender}</p>
+            </>
+          )
+        })
+      }
+      </Count>
+   </Content>
+
+  )
+}
+export default Example3
